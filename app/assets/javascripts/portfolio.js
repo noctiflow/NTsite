7902f1253f272d404a520714b122f406324adf9b
Richard Wang <rygoon@gmail.com>
2018-01-26 16:10:52 -0800

    added portfolio page

---
+++ app/assets/javascripts/portfolio.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      $('.carousel').carousel({
+            // dist: -20,
+            // shift:0,
+            padding:40,
+            indicators: true,
+
+      });
+
+      $('.modal').modal();
+
+    });
