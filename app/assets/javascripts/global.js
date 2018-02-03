c8ac79e6a192fb7bde4c46c8dc4232aab7f46163
dextrizzle <alfonzo_01@hotmail.com>
2018-02-02 16:51:05 -0800

    add js/html for navbar brand to fadein

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -16,31 +16,40 @@
 // });
 
 // NAVBAR JQUERY========================
-
+var triggered = false;
 $(document).ready(function() {
   function checkScroll(){
-      var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
-      if($(window).scrollTop() > startY){
-          $('.navbar').addClass("scrolled");
-      }else{
-          $('.navbar').removeClass("scrolled");
-      }
+    if($(window).scrollTop() > startY){
+      $('.navbar').addClass("scrolled");
+    }else{
+      $('.navbar').removeClass("scrolled");
+    }
   }
 
   if($('.navbar').length > 0){
-      $(window).on("scroll load resize", function(){
-          checkScroll();
-      });
+    $(window).on("scroll load resize", function(){
+      checkScroll();
+
+      // load logo in navbar once scrolling past splash ========================
+      if($('.navbar-nav > li').hasClass('active')){
+        if(!triggered) {
+          $('.navbar-header > a > img').addClass('animated fadeIn');
+          triggered=true;
+        }
+      }
+
+    });
   }
 
-    $('.footer2').on('mouseenter',function(event){
-      $('.footer').css('visibility','visible')
-      $('.footer2').css('visibility','hidden')
-    })
-    $('.footer').on('mouseleave',function(event){
-      $('.footer2').css('visibility','visible')
-      $('.footer').css('visibility','hidden')
-    })
+  $('.footer2').on('mouseenter',function(event){
+    $('.footer').css('visibility','visible')
+    $('.footer2').css('visibility','hidden')
+  })
+  $('.footer').on('mouseleave',function(event){
+    $('.footer2').css('visibility','visible')
+    $('.footer').css('visibility','hidden')
+  })
 
 });
