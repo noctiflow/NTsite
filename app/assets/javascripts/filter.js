26235831a44f24c8a02bdd762c7e629a4f64c2f5
dextrizzle <alfonzo_01@hotmail.com>
2018-02-12 17:55:20 -0800

    fix button with i18n problem and redesign inner content

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -3,22 +3,22 @@ $(document).ready(function(){
   $('.mission').show();
   $('.filtermenu > li').on('click',function(){
     console.log($(this).html());
-    if($(this).html()=='Mission' && !($(this).hasClass('filterbtn'))){
+    if($(this).hasClass('bt1') && !($(this).hasClass('filterbtn'))){
       $('.post').hide();
       $('li').removeClass('z-depth-5 filterbtn');
       $('.mission').fadeIn();
       $(this).addClass('z-depth-5 filterbtn');
-    }else if($(this).html()=='History' && !($(this).hasClass('filterbtn'))){
+    }else if($(this).hasClass('bt2') && !($(this).hasClass('filterbtn'))){
       $('.post').hide();
       $('li').removeClass('z-depth-5 filterbtn');
       $('.history').fadeIn();
       $(this).addClass('z-depth-5 filterbtn');
-    }else if($(this).html()=='Concept' && !($(this).hasClass('filterbtn'))){
+    }else if($(this).hasClass('bt3') && !($(this).hasClass('filterbtn'))){
       $('.post').hide();
       $('li').removeClass('z-depth-5 filterbtn');
       $('.concept').fadeIn();
       $(this).addClass('z-depth-5 filterbtn');
-    }else if($(this).html()=='Question' && !($(this).hasClass('filterbtn'))){
+    }else if($(this).hasClass('bt4') && !($(this).hasClass('filterbtn'))){
       $('.post').hide();
       $('li').removeClass('z-depth-5 filterbtn');
       $('.question').fadeIn();
