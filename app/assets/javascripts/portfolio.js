462fd1b10c4cbfa2b88571b64814f21d3bbc3227
dextrizzle <alfonzo_01@hotmail.com>
2018-02-22 03:19:16 -0800

    fix columns being too large on safari

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
       //
       // $('.modal').modal();
 
-      $(document).ready(function(){
-  $('.collapsible').collapsible();
-});
+//       $(document).ready(function(){
+//   $('.collapsible').collapsible();
+// });
 
     });
