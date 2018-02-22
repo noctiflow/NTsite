5d66100387eefaa6c90a1efd91d41ce80da652ce
dextrizzle <alfonzo_01@hotmail.com>
2018-02-21 18:06:29 -0800

    contact form functionality

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,16 @@
 class ContactsController < ApplicationController
   def new
-  @contact = Contact.new
-end
+    @contact = Contact.new
+  end
 
-def create
-  @contact = Contact.new(params[:contact])
-  @contact.request = request
-  if @contact.deliver
-    flash.now[:notice] = "#{ t('text_35') }"
-  else
-    flash.now[:error] = "#{ t('text_36') }"
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash[:notice] = "#{ t('text_35') }"
+    else
+      flash[:error] = "#{ t('text_36') }"
+    end
     redirect_to root_path(anchor: 'contact_us')
   end
-end
 end
